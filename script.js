d2cf8c8a88f36e050bc71026cb02034a392b8432
azbinfahmi <azbinfahmi@gmail.com>
2024-03-08 14:04:24 +0800

    b

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const modifiedFeatures = [];
 // Function to handle file input change
 function handleFileInputChange(event) {
     const file = event.target.files[0];
@@ -8,55 +9,49 @@ function handleFileInputChange(event) {
     reader.onload = function(event) {
       const content = event.target.result;
       const geojson = JSON.parse(content);
-      const focPattern = /FOC\d+/g;
-      
+      const focPattern = /FOC\s*-?\s*(\d+)/g;      
       const geojsonTable = document.getElementById('geojsonTable');
       geojsonTable.querySelector('tbody').innerHTML = '';
-      const modifiedFeatures = [];
-      // Process each feature
+
       let index = 0
-      let valueBefore, valueAfter
       geojson.features.forEach(feature => {
-        // Check if "Name" property contains "FOC" followed by a number
         if (feature.properties.Name && focPattern.test(feature.properties.Name)) {
           const matches = feature.properties.Name.match(focPattern);
-          // valueBefore = feature.properties.Name 
-          valueBefore = `Name: ${feature.properties.Name}`
           feature.properties.Name2 = matches[0]
           feature.properties.Name3 = feature.properties.Name
         }
-        // Check if "ID" property contains "FOC" followed by a number
         else if (feature.properties.ID && focPattern.test(feature.properties.ID)) {
           const matches = feature.properties.ID.match(focPattern);
-          //valueBefore = feature.properties.ID
-          valueBefore = `ID: ${feature.properties.ID}`
           feature.properties.Name2 = matches[0]
           feature.properties.Name3 = feature.properties.ID
         }
         else{
           if(feature.properties.Name != undefined){
-            valueBefore = `Name: ${feature.properties.Name}`
             feature.properties.Name2 = feature.properties.Name
             feature.properties.Name3 = feature.properties.Name
           }
           else if(feature.properties.ID != undefined){
-            valueBefore = `ID: ${feature.properties.ID}`
             feature.properties.Name2 = feature.properties.ID
             feature.properties.Name3 = feature.properties.ID
           }
           else{
-            valueBefore = 'none'
             feature.properties.Name2 = ''
             feature.properties.Name3 = ''
           }
         }
         const newRow = document.createElement('tr');
-        valueAfter = feature.properties.Name2
+        let IDName = feature.properties.ID
+        let Name = feature.properties.Name
+        let Name2 = feature.properties.Name2
+        let Name3 = feature.properties.Name3
+
         // Set row content
         newRow.innerHTML = `
           <td>${index + 1}</td>
-          <td>${valueBefore}</td>
-          <td>${valueAfter}</td>
+          <td>${IDName}</td>
+          <td>${Name}</td>
+          <td>${Name2}</td>
+          <td>${Name3}</td>
         `;
         // Append the new row to the table
         geojsonTable.querySelector('tbody').appendChild(newRow);        
@@ -65,15 +60,16 @@ function handleFileInputChange(event) {
         modifiedFeatures.push(modifiedFeature);
       });
       // Display the table if it has rows
-      geojsonTable.style.display = geojsonTable.querySelector('tbody').children.length > 0 ? 'block' : 'none';
-      downloadGeoJSON(modifiedFeatures);
+      geojsonTable.style.display = geojsonTable.querySelector('tbody').children.length > 0 ? 'block' : 'none'; //width: 100%;
+      //downloadGeoJSON(modifiedFeatures);
       console.log(geojson);
     };
     reader.readAsText(file);
 }
   
 // Function to download GeoJSON file
-function downloadGeoJSON(data) {
+function downloadGeoJSON() {
+  data = modifiedFeatures
   const json = JSON.stringify({ type: "FeatureCollection", features: data });
   const blob = new Blob([json], { type: "application/json" });
   const a = document.createElement("a");
@@ -84,6 +80,9 @@ function downloadGeoJSON(data) {
     a.click();
   }
 }
-  // Add event listener for file input change
-  document.getElementById('fileInput').addEventListener('change', handleFileInputChange);
-  
+
+// Add event listener for file input change
+document.getElementById('fileInput').addEventListener('change', handleFileInputChange);
+
+const downloadButton = document.getElementById('downloadButton');
+downloadButton.addEventListener('click', downloadGeoJSON);
