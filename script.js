3cd42694b0c228e29df2e3d6c68894eb38c56b15
azbinfahmi <azbinfahmi@gmail.com>
2024-03-13 07:32:51 +0800

    add foc name

--- script.js
+++ script.js
@@ -1,71 +1,72 @@
 let modifiedFeatures = [];
 // Function to handle file input change
 function handleFileInputChange(event) {
-    modifiedFeatures = []
-    const file = event.target.files[0];
-    if (!file) return;
-  
-    // Read the file as text
-    const reader = new FileReader();
-    reader.onload = function(event) {
-      const content = event.target.result;
-      const geojson = JSON.parse(content);
-      const focPattern = /FOC\s*-?\s*(\d+)/g;      
-      const geojsonTable = document.getElementById('geojsonTable');
-      geojsonTable.querySelector('tbody').innerHTML = '';
+  modifiedFeatures = []
+  const file = event.target.files[0];
+  if (!file) return;
 
-      let index = 0
-      geojson.features.forEach(feature => {
-        if (feature.properties.Name && focPattern.test(feature.properties.Name)) {
-          const matches = feature.properties.Name.match(focPattern);
-          feature.properties.Name2 = matches[0]
-          feature.properties.Name3 = feature.properties.Name
+  // Read the file as text
+  const reader = new FileReader();
+  reader.onload = function(event) {
+    const frontName = prompt("Enter a file name:", "F1");
+    const content = event.target.result;
+    const geojson = JSON.parse(content);
+    const focPattern = /FOC\s*-?\s*(\d+)/g;      
+    const geojsonTable = document.getElementById('geojsonTable');
+    geojsonTable.querySelector('tbody').innerHTML = '';
+
+    let index = 0
+    geojson.features.forEach(feature => {
+      if (feature.properties.Name && focPattern.test(feature.properties.Name)) {
+        const matches = feature.properties.Name.match(focPattern);
+        feature.properties.Name2 = matches[0]
+        feature.properties.Name3 = frontName + '= ' + feature.properties.Name
+      }
+      else if (feature.properties.ID && focPattern.test(feature.properties.ID)) {
+        const matches = feature.properties.ID.match(focPattern);
+        feature.properties.Name2 = matches[0]
+        feature.properties.Name3 = frontName + '= ' + feature.properties.ID
+      }
+      else{
+        if(feature.properties.Name != undefined){
+          feature.properties.Name2 = feature.properties.Name
+          feature.properties.Name3 = frontName + '= ' + feature.properties.Name
         }
-        else if (feature.properties.ID && focPattern.test(feature.properties.ID)) {
-          const matches = feature.properties.ID.match(focPattern);
-          feature.properties.Name2 = matches[0]
-          feature.properties.Name3 = feature.properties.ID
+        else if(feature.properties.ID != undefined){
+          feature.properties.Name2 = feature.properties.ID
+          feature.properties.Name3 = frontName + '= ' + feature.properties.ID
         }
         else{
-          if(feature.properties.Name != undefined){
-            feature.properties.Name2 = feature.properties.Name
-            feature.properties.Name3 = feature.properties.Name
-          }
-          else if(feature.properties.ID != undefined){
-            feature.properties.Name2 = feature.properties.ID
-            feature.properties.Name3 = feature.properties.ID
-          }
-          else{
-            feature.properties.Name2 = ''
-            feature.properties.Name3 = ''
-          }
+          feature.properties.Name2 = ''
+          feature.properties.Name3 = ''
         }
-        const newRow = document.createElement('tr');
-        let IDName = feature.properties.ID
-        let Name = feature.properties.Name
-        let Name2 = feature.properties.Name2
-        let Name3 = feature.properties.Name3
+      }
+      const newRow = document.createElement('tr');
+      let IDName = feature.properties.ID
+      let Name = feature.properties.Name
+      let Name2 = feature.properties.Name2
+      let Name3 = feature.properties.Name3
 
-        // Set row content
-        newRow.innerHTML = `
-          <td>${index + 1}</td>
-          <td>${IDName}</td>
-          <td>${Name}</td>
-          <td>${Name2}</td>
-          <td>${Name3}</td>
-        `;
-        // Append the new row to the table
-        geojsonTable.querySelector('tbody').appendChild(newRow);        
-        index += 1
-        const modifiedFeature = { ...feature, properties: { ...feature.properties} };
-        modifiedFeatures.push(modifiedFeature);
-      });
-      // Display the table if it has rows
-      geojsonTable.style.display = geojsonTable.querySelector('tbody').children.length > 0 ? 'block' : 'none'; //width: 100%;
-      //downloadGeoJSON(modifiedFeatures);
-      console.log(geojson);
-    };
-    reader.readAsText(file);
+      // Set row content
+      newRow.innerHTML = `
+        <td>${index + 1}</td>
+        <td>${IDName}</td>
+        <td>${Name}</td>
+        <td>${Name2}</td>
+        <td>${Name3}</td>
+      `;
+      // Append the new row to the table
+      geojsonTable.querySelector('tbody').appendChild(newRow);        
+      index += 1
+      const modifiedFeature = { ...feature, properties: { ...feature.properties} };
+      modifiedFeatures.push(modifiedFeature);
+    });
+    // Display the table if it has rows
+    geojsonTable.style.display = geojsonTable.querySelector('tbody').children.length > 0 ? 'block' : 'none'; //width: 100%;
+    //downloadGeoJSON(modifiedFeatures);
+    console.log(geojson);
+  };
+  reader.readAsText(file);
 }
   
 // Function to download GeoJSON file
